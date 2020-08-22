3447d011c3141563d5879eae9db1bdca3f565d14
Vivek Kumar Chaurasia <vivek.chaurasia@synapse.com>
2020-08-22 12:30:26 +0530

    console remove

--- src/actions/ContactAction.js
+++ src/actions/ContactAction.js
@@ -4,6 +4,7 @@ export const getContact = () => (dispatch) => {
     const axios = require('axios');
     axios.get("http://localhost:3000/contacts")
         .then((response) => {
+            console.log(response.data);
             dispatch({ type: GET_CONTACT, payload: response.data });
         })
 };
